2adc0d32ff7a5fad988609206229edd6bd1f793d
adib-the-noob <mdadib550@gmail.com>
2023-09-12 22:27:11 +0600

    Get todos added

--- todoApp/app.py
+++ todoApp/app.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, Depends
-from typing import Annotated, List
+from typing import Annotated, List, Optional
 from pydantic import BaseModel
 
 from sqlalchemy.orm import Session
@@ -11,10 +11,10 @@ app = FastAPI()
 models.Base.metadata.create_all(bind=engine)
 
 
-
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
 
 def get_db():
     db = SessionLocal()
@@ -23,8 +23,16 @@ def get_db():
     finally:
         db.close()
 
+
 class Todos(BaseModel):
-    id : int
+    id: Optional[int]
+    title: str
+    description: str
+    status: bool
+
+
+class TodosInDB(Todos):
+    id: int
     title: str
     description: str
     status: bool
@@ -32,16 +40,16 @@ class Todos(BaseModel):
 
 @app.post("/create-todo")
 async def create_todos(todo: Todos, db: Annotated[Session, Depends(get_db)]):
-    new_todo = models.Todos(title=todo.title, description=todo.description, status=todo.status)
+    new_todo = models.Todos(
+        title=todo.title, description=todo.description, status=todo.status
+    )
     db.add(new_todo)
     db.commit()
     db.refresh(new_todo)
-    return {
-        "data": new_todo
-    }
+    return {"data": new_todo}
 
 
-@app.get('/all-todos', response_model=List[Todos])
-async def get_all_todos(db : Annotated[Session, Depends(get_db)]):
+@app.get("/all-todos", response_model=List[TodosInDB])
+async def get_all_todos(db: Annotated[Session, Depends(get_db)]):
     todos = db.query(models.Todos).all()
     return todos
